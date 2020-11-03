9ad1e60e33dc32693625a49443016c0dac4f04b7
Ben Orgera <benorg@me.com>
2020-11-02 20:25:42 -0500

    Added search as you type, removed submit button for searching via css

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import { Layout } from "@elastic/react-search-ui-views";
 
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
 
+import css from './style.css';
+
 const connector = new AppSearchAPIConnector({
   searchKey: "search-371auk61r2bwqtdzocdgutmg",
   engineName: "search-ui-examples",
@@ -18,9 +20,9 @@ export default function App() {
         apiConnector: connector
       }}
     >
-      <div className="App">
+      <div className="App" style={css}>
         <Layout
-          header={<SearchBox />}
+          header={<SearchBox searchAsYouType={true}/>}
           bodyContent={<Results titleField="title" urlField="nps_link" />}
         />
       </div>
